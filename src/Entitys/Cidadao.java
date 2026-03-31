482b9a03d16ff76c4a108d03da9040ed52392cad
vitoria-la <laura.v.fabiani@gmail.com>
2026-03-31 10:47:50 -0300

    Adicionando o formatador de CPF

--- src/Entitys/Cidadao.java
+++ src/Entitys/Cidadao.java
@@ -4,8 +4,29 @@ import java.util.List;
 
 public class Cidadao extends Usuario{
     private String nome;
-    private String cpf;
     private boolean ehAnonimo;
     private List<Ticket> listaDeTickets;
 
+    public String getCpfFormatado() {
+        String cpf = super.getCpf();
+        String cpfFormatado = "";
+
+        int x = 0;
+        do {
+            if (cpfFormatado.length() == 3 || cpfFormatado.length() == 7) {
+                cpfFormatado += ".";
+            } else if (cpfFormatado.length() == 10) {
+                cpfFormatado += "-";
+            } else {
+                cpfFormatado += cpf.charAt(x);
+                x++;
+            }
+        } while (cpfFormatado.length() != 13);
+
+        // 0 1 2 . 3 4 5 . 6 7 8 - 9 0
+        return cpfFormatado;
+    }
+
 }
+
+
